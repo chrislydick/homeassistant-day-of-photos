6e085e40361db124dcf335e8b9f8b4207c42a699
Chris Lydick <chris.lydick@gmail.com>
2024-06-12 18:19:24 -0700

    Update run_script.bash

--- run_script.bash
+++ run_script.bash
@@ -4,6 +4,6 @@ rm -f /home/pi/images/*
 
 python /home/pi/rotate_images.py
 
-ssh -p 22222 root@homeassistant.local 'rm -f /media/*.jpg'
+ssh -p 22222 root@192.168.1.146 'rm -f /media/*.jpg'
 
-scp -P 22222 /home/pi/images/*.jpg root@homeassistant.local:/media/.
+scp -P 22222 /home/pi/images/*.jpg root@192.168.1.146:/media/.
